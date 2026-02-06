89ad647d678e10a13fe365f9ce5a5d7463fe5512
Robinbisht394 <robinbisht394@gmail.com>
2026-02-06 12:54:43 +0530

    fix : auth function import error

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -3,12 +3,12 @@ const userController = require("../Controller/user.controller.js");
 const userValidation = require("../validations/user.validations.js");
 const router = express.Router();
 const validate = require("../middlewares/validate.middleware.js");
-const { protect, roleCheck } = require("../middlewares/auth.middleware.js");
+const { protect, userRole } = require("../middlewares/auth.middleware.js");
 router.get("/", userController.getUsers);
 router.post(
   "/changepassword",
   protect,
-  roleCheck,
+  userRole,
   validate(userValidation.changeUserPasswordSchema),
 
   userController.changeUserPassword,
