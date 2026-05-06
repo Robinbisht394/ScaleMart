cffd1c107bc4e0e9bb47aafd4055464ff21fbe13
Robinbisht394 <robinbisht394@gmail.com>
2026-05-06 11:47:36 +0530

    Removed Redis references temporarily from the codebase

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./Config/database.js");
-const { connectRedis } = require("./Config/redis.js");
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-connectRedis();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
